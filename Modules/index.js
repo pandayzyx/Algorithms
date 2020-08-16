dede2f16c7d9e9272ae6ad97fa32521f912d6a13
pandayzyx <gauravx.gp@gmail.com>
2020-08-16 23:41:43 +0530

    closures

---
+++ Modules/index.js
@@ -0,0 +1,11 @@
+// export function sum(){
+//     let a =   5
+//     return a
+// }
+
+
+
+
+
+
+
--- revision.js
+++ revision.js
@@ -1,2 +1,39 @@
+//closures
+// function sum(count){
+//     let counter  =  count
+//    function add(a){
+//        return counter = counter+a
+//    }
+//    function sub(b){
+//        return counter-=b
+//    }
+//    function mul(c){
+//        return counter *=c
+//    }
+//    return{add,sub,mul}
+
+// }
+// var first =  sum(2)
+// console.log(first)
+// console.log(first.add(3))
+// console.log(first.sub(1))
+// console.log(first.mul(2))
+
+
+//closures
+// function sum(a){
+//     let counter  =  a
+//     return function(b){
+//         return function(c){
+//             return function(d){
+//                 return counter+b+c+d
+//             }
+//         }
+//     }
+// }
+// let ans =  sum(1)(2)(3)(4)
+// console.log(ans)
+
+
 
 
